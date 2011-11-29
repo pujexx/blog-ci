59af4b9d80cfcccd986e6d8dc7e36383178893e4
Puji Rahmadiyanto <puji.rahmadiyanto@gmail.com>
2011-11-29 13:36:55 +0700

    template

---
+++ application/controllers/home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        $this->load->view("template");
+    }
+
+}
+?>
